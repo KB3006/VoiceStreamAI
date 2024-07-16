2989c9260f6ad508a80ab804099d494d212e5888
KB3006 <kalyan@atirath.com>
2024-07-16 15:22:32 +0530

    serve html

---
+++ client/serverHtml.py
@@ -0,0 +1,20 @@
+import http.server
+import socketserver
+
+PORT = 5000
+DIRECTORY = "."
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path in ["/StreamTranscription", "/StreamTranscription/"]:
+            self.path = "index.html"
+        return super().do_GET()
+
+    def translate_path(self, path):
+        # Adjust the path to serve files from the DIRECTORY
+        path = super().translate_path(path)
+        return path.replace("/StreamTranscription", DIRECTORY, 1)
+
+with socketserver.TCPServer(("", PORT), MyHttpRequestHandler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
